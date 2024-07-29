92e0341a0ede5303fffa8cf1bd998d8fd88a5edf
Thirunavukkarasu T <97720002+thiru042004@users.noreply.github.com>
2024-07-29 22:54:31 +0530

    ADD. Added test with unequal values, and checks for in equality.

--- src/test/java/com/probability/ProbabilityTest.java
+++ src/test/java/com/probability/ProbabilityTest.java
@@ -2,8 +2,7 @@ package com.probability;
 
 import org.junit.jupiter.api.Test;
 
-import static org.hamcrest.CoreMatchers.equalTo;
-import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.*;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ProbabilityTest {
@@ -15,5 +14,14 @@ public class ProbabilityTest {
         Probability probabilityOfTail = new Probability(0.5);
 
         assertThat(probabilityOfHead, is(equalTo(probabilityOfTail)));
+    }
+
+    @Test
+    void toCheckInEqualityWhenTheProbabilityOfGettingHeadIsComparedWithProbabilityOfOneInADice() {
+
+        Probability probabilityOfHead = new Probability(0.5);
+        Probability probabilityOfOneInADice = new Probability(0.167);
+
+        assertThat(probabilityOfHead, is(not(equalTo(probabilityOfOneInADice))));
     }
 }
