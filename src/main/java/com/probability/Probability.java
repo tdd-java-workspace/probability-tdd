d1811eafe01d5739f2d8ef083e6e835486064321
Thirunavukkarasu T <97720002+thiru042004@users.noreply.github.com>
2024-07-29 22:34:37 +0530

    ADD. Added test to check the equality of probability of head and tail.

--- src/main/java/com/probability/Probability.java
+++ src/main/java/com/probability/Probability.java
@@ -2,4 +2,10 @@ package com.probability;
 
 public class Probability {
 
+    double probabilityValue;
+
+    public Probability(double probabilityValue) {
+        this.probabilityValue = probabilityValue;
+    }
+
 }
--- src/test/java/com/probability/ProbabilityTest.java
+++ src/test/java/com/probability/ProbabilityTest.java
@@ -1,6 +1,19 @@
 package com.probability;
 
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
 public class ProbabilityTest {
 
+    @Test
+    void toCheckEqualityWhenTheProbabilityOfGettingHeadInACoinIsComparedWithTheProbabilityOfGettingTailInACoin() {
 
+        Probability probabilityOfHead = new Probability(0.5);
+        Probability probabilityOfTail = new Probability(0.5);
+
+        assertThat(probabilityOfHead, is(equalTo(probabilityOfTail)));
+    }
 }
